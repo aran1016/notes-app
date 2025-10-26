15521e7610b099932ae32b39b46114f3209e4d12
Ali <aliranjha987@gmail.com>
2025-10-26 18:23:33 -0400

    delete notes done

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -2,8 +2,26 @@ import { PenSquareIcon, Trash2Icon } from 'lucide-react'
 import React from 'react'
 import { Link } from 'react-router'
 import { formatDate } from '../lib/utils.js'
+import api from '../lib/axios.js'
+import toast from 'react-hot-toast'
 
-const NoteCard = ({ note }) => {
+const NoteCard = ({ note, setNotes }) => {
+
+    const handleDelete = async (e, id) => {
+        e.preventDefault(); // gets rid of the navigation behavior
+
+        if(!window.confirm("Are you sure you want to delete this note?")) return;
+        
+        try {
+            await api.delete(`/notes/${id}`);
+            setNotes((prev) => prev.filter(note => note._id !== id)); // get rid of the deleted note from array
+            toast.success("Note deleted successfully!");
+        } catch (error) {
+            toast.error("Failed to delete note");
+            console.log("Error deleting note:", error);
+        }
+    }
+
   return (
     <Link to={`/note/${note._id}`}
         className="card bg-bass-100 hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-[#00FF9D]"
@@ -17,7 +35,7 @@ const NoteCard = ({ note }) => {
                 </span>
                 <div className='flex items-center gap-1'>
                     <PenSquareIcon className='size-4' />
-                    <button className='btn btn-ghost btn-xs text-error'>
+                    <button className='btn btn-ghost btn-xs text-error' onClick={(e) => handleDelete(e, note._id)}>
                        <Trash2Icon className='size-4' />
                     </button>
                 </div>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import RateLimitedUI from '../components/RateLimitedUI'
+import NotesNotFound from '../components/NotesNotFound'
 import { useState } from 'react'
 import { useEffect } from 'react'
 import NoteCard from '../components/NoteCard'
@@ -43,10 +44,11 @@ const HomePage = () => {
     <div className='max-w-7xl mx-auto p-4 mt-6'>
         {loading && <div className='text-center text-primary py-10'> Loading Notes ... </ div>}
 
+        {notes.length === 0 && !loading && !isRateLimited && <NotesNotFound />}
         {notes.length > 0 && !isRateLimited && (
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
                 {notes.map(note => (
-                    <NoteCard key={note._id} note={note} />
+                    <NoteCard key={note._id} note={note} setNotes={setNotes} />
                 ))}
             </ div>
         )}
